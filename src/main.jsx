ee38fbac5db9dcefa05452fd2179072c25e23e34
Tamires Santos <tfs.tamsantos@gmail.com>
2023-07-31 13:26:35 -0300

    Estilo Global

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
+import GlobalStyles from './styles/global'
 
 import theme from './styles/theme'
 
@@ -9,6 +10,7 @@ import { Details } from './pages/Details'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyles />
       <Details />
     </ThemeProvider>
   </React.StrictMode>,
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  background: red;  
 `
