85ae9d216f39683556584ec9789add9925a574e1
Tamires Santos <tfs.tamsantos@gmail.com>
2023-07-31 14:35:00 -0300

    Fontes personalizados

--- src/styles/global.js
+++ src/styles/global.js
@@ -11,6 +11,13 @@ export default createGlobalStyle`
     background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
     color: ${({ theme }) => theme.COLORS.WHITE};
 
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button, textarea {
+    font-family: 'Roboto Slab', serif;
+    font-size: 16px;
+    outline: none;
   }
 
   a {
