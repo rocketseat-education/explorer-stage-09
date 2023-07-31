2ca602c8a82914f680803df3bb1f001a9d3aa5a6
Tamires Santos <tfs.tamsantos@gmail.com>
2023-07-31 11:15:09 -0300

    Tema

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
+
+import theme from './styles/theme'
+
 import { Details } from './pages/Details'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Details />
+    <ThemeProvider theme={theme}>
+      <Details />
+    </ThemeProvider>
   </React.StrictMode>,
 )
