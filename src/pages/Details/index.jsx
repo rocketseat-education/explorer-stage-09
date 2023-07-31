867b89746bcd6ed845ead893e94c053d1716aaeb
Tamires Santos <tfs.tamsantos@gmail.com>
2023-07-31 14:46:57 -0300

    Componente

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,10 +1,14 @@
-import { Container } from "./styles"
+import { Container } from './styles'
+
+import { Button } from '../../components/Button'
 
 export function Details() {
   return (
     <Container>
       <h1>Hello World!</h1>
       <span>Rodrigo Gonçalves</span>
+
+      <Button />
     </Container>
   )
 }
