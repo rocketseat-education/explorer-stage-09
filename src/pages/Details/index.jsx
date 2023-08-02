1ac4f76ca60ec214d907494a49f4025e8691c9a1
Tamires Santos <tfs.tamsantos@gmail.com>
2023-08-02 13:35:26 -0300

    Componente Tag

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -3,6 +3,7 @@ import { Container, Links } from './styles'
 import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
 import { Section } from '../../components/Section'
+import { Tag } from '../../components/Tag'
 
 export function Details() {
   return (
@@ -18,6 +19,11 @@ export function Details() {
             <a href="#">https://rocketseat.com.br</a>
           </li>
         </Links>
+      </Section>
+
+      <Section title="Marcadores">
+        <Tag title="express" />
+        <Tag title="node" />
       </Section>
 
       <Button title="Voltar" />
