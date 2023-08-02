0032aa54a2dc5e8efde97360af7011eb2d0823c1
Tamires Santos <tfs.tamsantos@gmail.com>
2023-08-02 13:58:21 -0300

    Componente ButtonText

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,14 +1,17 @@
 import { Container, Links } from './styles'
 
+import { Tag } from '../../components/Tag'
 import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
 import { Section } from '../../components/Section'
-import { Tag } from '../../components/Tag'
+import { ButtonText } from '../../components/ButtonText'
 
 export function Details() {
   return (
     <Container>
       <Header />
+
+      <ButtonText title="Excluir nota" />
 
       <Section title="Links úteis">
         <Links>
