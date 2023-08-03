c2291abcd02b54bad73b3de464ae6ab58f4440a7
Tamires Santos <tfs.tamsantos@gmail.com>
2023-08-03 12:44:30 -0300

    Aplicando Grid na Home

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,13 @@ import GlobalStyles from './styles/global'
 
 import theme from './styles/theme'
 
-import { Details } from './pages/Details'
+import { Home } from './pages/Home'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Details />
+      <Home />
     </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,31 @@
+import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
+
+import { Header } from '../../components/Header'
+
+export function Home() {
+  return (
+    <Container>
+      <Brand>
+        <h1>Rocketnotes</h1>
+      </Brand>
+
+      <Header></Header>
+
+      <Menu>
+
+      </Menu>
+
+      <Search>
+
+      </Search>
+
+      <Content>
+
+      </Content>
+
+      <NewNote>
+
+      </NewNote>
+    </Container>
+  )
+}
