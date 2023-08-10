4168a8bd05041401484cdb4ee17ea8ab372e16c8
Tamires Santos <tfs.tamsantos@gmail.com>
2023-08-10 13:45:00 -0300

    Input

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,6 +2,7 @@ import { FiPlus } from 'react-icons/fi'
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
 
 import { Header } from '../../components/Header'
+import { Input } from '../../components/Input'
 import { ButtonText } from '../../components/ButtonText'
 
 export function Home() {
@@ -20,7 +21,7 @@ export function Home() {
       </Menu>
 
       <Search>
-
+        <Input placeholder="Pesquisar pelo título" />
       </Search>
 
       <Content>
