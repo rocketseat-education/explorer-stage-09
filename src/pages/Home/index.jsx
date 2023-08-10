e95b6d9e8f7c4ba5468281cd8be33239a766ff85
Tamires Santos <tfs.tamsantos@gmail.com>
2023-08-10 13:18:21 -0300

    Botão Criar Nota

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,4 @@
+import { FiPlus } from 'react-icons/fi'
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
 
 import { Header } from '../../components/Header'
@@ -27,7 +28,8 @@ export function Home() {
       </Content>
 
       <NewNote>
-
+        <FiPlus />
+        Criar nota
       </NewNote>
     </Container>
   )
