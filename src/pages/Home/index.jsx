111a4447a79eed4e8ba9ef00e32c07df950116e6
Tamires Santos <tfs.tamsantos@gmail.com>
2023-08-14 08:49:01 -0300

    Componente de Notas

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,10 @@
 import { FiPlus } from 'react-icons/fi'
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
 
-import { Header } from '../../components/Header'
+import { Note } from '../../components/Note'
 import { Input } from '../../components/Input'
+import { Header } from '../../components/Header'
+import { Section } from '../../components/Section'
 import { ButtonText } from '../../components/ButtonText'
 
 export function Home() {
@@ -25,7 +27,16 @@ export function Home() {
       </Search>
 
       <Content>
-
+        <Section title="Minhas notas">
+          <Note data={{
+            title: 'React',
+            tags: [
+              { id: '1', name: 'react' },
+              { id: '2', name: 'rocketseat' }
+            ]
+          }}
+          />
+        </Section>
       </Content>
 
       <NewNote>
