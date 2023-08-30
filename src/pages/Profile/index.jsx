bff6aaa48039c3e12f0b1272033b1f229a114003
Tamires Santos <tfs.tamsantos@gmail.com>
2023-08-30 08:56:49 -0300

    Profile

---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,46 @@
+import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi'
+
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
+
+import { Container, Form } from "./styles";
+
+export function Profile() {
+  return (
+    <Container>
+      <header>
+        <a href="/">
+          <FiArrowLeft />
+        </a>
+      </header>
+
+      <Form>
+        <Input
+          placeholder="Nome"
+          type="text"
+          icon={FiUser}
+        />
+
+        <Input
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+        />
+
+        <Input
+          placeholder="Senha atual"
+          type="password"
+          icon={FiLock}
+        />
+
+        <Input
+          placeholder="Nova atual"
+          type="password"
+          icon={FiLock}
+        />
+
+        <Button title="Salvar" />
+      </Form>
+    </Container>
+  )
+}
