3c124275a2b02870f58f3cf5ab8b996edbc0df95
Tamires Santos <tfs.tamsantos@gmail.com>
2023-08-30 10:24:14 -0300

    Finalizando o New

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,8 +1,9 @@
-import { Header } from '../../components/Header'
-import { Input } from '../../components/Input'
 import { Textarea } from '../../components/Textarea'
 import { NoteItem } from '../../components/NoteItem'
 import { Section } from '../../components/Section'
+import { Button } from '../../components/Button'
+import { Header } from '../../components/Header'
+import { Input } from '../../components/Input'
 
 import { Container, Form } from './styles'
 
@@ -21,10 +22,19 @@ export function New() {
           <Input placeholder="Título" />
           <Textarea placeholder="Observações" />
 
-          <Section title="links úteis">
+          <Section title="Links úteis">
             <NoteItem value="https://rocketseat.com.br" />
             <NoteItem isNew placeholder="Novo link" />
           </Section>
+
+          <Section title="Marcadores">
+            <div className="tags">
+              <NoteItem value="react" />
+              <NoteItem isNew placeholder="Nova tag" />
+            </div>
+          </Section>
+
+          <Button title="Salvar" />
         </Form>
       </main>
     </Container>
