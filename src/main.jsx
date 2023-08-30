f643a0998582522cca75118d74f30054fe321717
Tamires Santos <tfs.tamsantos@gmail.com>
2023-08-30 09:21:06 -0300

    Interface New

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,13 @@ import GlobalStyles from './styles/global'
 
 import theme from './styles/theme'
 
-import { Profile } from './pages/Profile'
+import { New } from './pages/New'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Profile />
+      <New />
     </ThemeProvider>
   </React.StrictMode>,
 )
