d834a876eddb104ba6c5654833d95cd67dedf59c
Tamires Santos <tfs.tamsantos@gmail.com>
2023-08-30 09:26:57 -0300

    Header da New

--- src/pages/New/styles.js
+++ src/pages/New/styles.js
@@ -10,3 +10,21 @@ export const Container = styled.div`
   "header"
   "content";
 `
+
+export const Form = styled.form`
+  max-width: 550px;
+  margin: 38px auto;
+
+  > header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    margin-bottom: 36px;
+
+    a {
+      font-size: 20px;
+      color: ${({ theme }) => theme.COLORS.GRAY_100}
+    }
+  }
+`
