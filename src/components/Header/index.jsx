e95a463923c89972f53596a1b561dc0c5be87307
Tamires Santos <tfs.tamsantos@gmail.com>
2023-08-30 14:07:51 -0300

    Navegação

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,7 +4,7 @@ import { Container, Profile, Logout } from './styles'
 export function Header() {
   return (
     <Container>
-      <Profile>
+      <Profile to="/profile">
         <img
           src="https://github.com/rodrigorgtic.png"
           alt="Foto do usuário"
